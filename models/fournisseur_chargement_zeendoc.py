ecc9e092d48e12603567291454fcd3719940b61e
Hermez Dalil <hermez.dalil@gmail.com>
2024-03-10 12:18:30 +0100

    ajout des champs de la ligne dans la notification spinet

--- models/fournisseur_chargement_zeendoc.py
+++ models/fournisseur_chargement_zeendoc.py
@@ -14,20 +14,31 @@ class fournisseur_chargement_zeendoc(models.Model):
 
     def synchronisation_function(self):
         # suppliers = self.env['res.partner'].search([("supplier_rank",">",0)])
-
+        # length = suppliers.length()
         # for supplier in suppliers
-        # ValidationError(_('voulez-vous vraiment ?'))
+        #    message2 = message2 + str(supplier.city) + "/ Supplyer "
+
+        name = "  name = " + str(self.display_name)
+        city = " city = " + str(self.city)
+        country = " country = " + str(self.country_id)
+        reference = " reference = " + str(self.ref)
+        siret = " siret = " + str(self.siret)
+        phone = " phone = " + str(self.phone)
+        email = " email = " + str(self.email)
+        zip = " zip = " + str(self.zip)
+        id = " id = " + str(self.id)
+
         url = "https://geo.api.gouv.fr/communes?"
         params = {
             'codePostal': '78000'
         }
         reponse = requests.get(url, params=params)
-        message = "Mise-a-jour avec succes : " + str(reponse.json())
+        # message = "Mise-a-jour avec succes : " + str(reponse.json())
         return {
             'type': 'ir.actions.client',
             'tag': 'display_notification',
             'params': {
-                'message': message,
+                'message': id + name + city + country + reference + siret + phone + email + zip,
                 'type': 'success',
                 'sticky': False,
             }
@@ -35,9 +46,12 @@ class fournisseur_chargement_zeendoc(models.Model):
 
         # res = super().create(vals)
 
+        # ValidationError(_('voulez-vous vraiment ?'))
+
         # {'search_default_supplier': 1,'res_partner_search_mode': 'supplier', 'default_is_company': True, 'default_supplier_rank': 1}
         # [["supplier_rank",">",0]]
 
         # return res
+
 
 
